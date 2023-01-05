3ade4524d1bf63b8339b4c13d3ee2f91d988b1f3
Mohamed Fathy <84096845+Mohamed-Fathy-62@users.noreply.github.com>
2023-01-05 22:05:58 +0200

    Add files via upload

--- UartTB.v
+++ UartTB.v
@@ -9,53 +9,56 @@ module UartTB();
   reg[31:0] PWDATA;
   reg PREADY;
   wire[31:0] PRDATA;
-  //-------------TX SIGNALS---------
-  wire tx;
-  wire Tx_Done;
-  reg Tx_Start = 1'b0;
+  //-------------RX SIGNALS---------
+  reg rx;
+  wire rx_Done;
   //--------------------------------
-
-  
   parameter c_CLOCK_PERIOD_NS = 40;
   wire tick;
+  
   BaudRate ss(PCLK,tick);
-            
-  UartTx  uarttx(.PWDATA(PWDATA),
-                 .Txstart(Tx_Start),
+  
+  UartRx  uartrx(.Rx(rx),
+                 .PCLK(PCLK),
                  .PRESETn(PRESETn),
                  .tick(tick),
-                 .PCLK(PCLK),
-                 .Txd(tx),  
-                 .TxD_done(Tx_Done)); 
-
-always
+                 .PRDATA(PRDATA),
+                 .RX_Done(rx_Done));
+                 
+                 
+  always
     #(c_CLOCK_PERIOD_NS/2) PCLK <= !PCLK;
-  initial begin
+    
+    
+initial begin
       @(posedge PCLK);
-      PADDR <= 32'h0001;
-      PWRITE <= 1'b1;
+      PADDR <= 32'h0002;
+      PWRITE <= 1'b0;
       PSEL <= 1'b1;
-      PWDATA = 32'h010e;
       @(posedge PCLK);
       PENABLE <= 1'b1;
       PREADY <=1'b1;
+      
       @(posedge PCLK);
       PRESETn <= 1'b0;
       @(posedge PCLK);
       @(posedge PCLK);
       PRESETn <= 1'b1;
-      //------------TX----------
+      //---------------RX-------------
       @(posedge PCLK);
-      @(posedge PCLK);
-      Tx_Start <= 1'b1;
-      @(posedge PCLK);
-      @(posedge PCLK);
-      Tx_Start <= 1'b0;
-      @(posedge Tx_Done);
+      rx <= 0;
+      repeat(6)@(posedge PCLK);
+      rx <= 1;
+      repeat(80)@(posedge PCLK);
+      rx<=0;
+
+      
+      @(posedge rx_Done);
+      rx <= 1'b1;
       PSEL <= 1'b0;
       PENABLE <= 1'b0;
       PREADY <=1'b0;
       PWRITE <= 1'b0;
-      //----------------------
+
 end
 endmodule
